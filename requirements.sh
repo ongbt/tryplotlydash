03399e03e78b97e02c30bc1776af62a0f1ffb131
ongbt <bengtiong@gmail.com>
2023-10-01 12:59:42 +0800

    Fixed

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,5 @@
 pip list --format=freeze > requirements.txt
+# Issues:
+# mkl_fft: https://github.com/jocelynpender/fna-query/issues/4
+# gunicorn: https://community.render.com/t/bash-gunicorn-command-not-found/1485
+# https://community.plotly.com/t/migrating-from-heroku-how-to-use-render-to-deploy-a-python-dash-app-solution/68048/2
